8fcae3b40ca3874a7d93008831f73b97becd2f1e
Michael Springer <mail@michaelspringer.net>
2013-05-30 22:16:03 -0400

    Init

---
+++ jsLoader.js
@@ -0,0 +1,160 @@
+/* ==========================================================================
+ jsLoader - a nifty javascript loader
+ author: Michael Springer <mike@sprngr.me>
+ version: 0.1
+ license: MIT License
+ ========================================================================== */
+
+//Set some helper vars and functions
+var head = document.getElementsByTagName("head")[0];
+
+//Borrowed polyfill from Mozilla
+//https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/String/endsWith
+if (!String.prototype.endsWith) {
+    Object.defineProperty(String.prototype, 'endsWith', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function (searchString, position) {
+            "use strict";
+            position = position || this.length;
+            position = position - searchString.length;
+            return this.lastIndexOf(searchString) === position;
+        }
+    });
+}
+
+//Trims whitespace
+if (typeof String.prototype.trim !== 'function') { // detect native implementation
+    String.prototype.trim = function () {
+        "use strict";
+        return this.replace(/^\s+/, '').replace(/\s+$/, '');
+    };
+}
+
+
+//The fun stuff
+jsLoader = {
+
+    debug: function () {
+        "use strict";
+        console.log("::: jsLoader Debug :::");
+
+        var jsTag = document.querySelectorAll('[data-type="jsLoader"]');
+        console.log("Valid jsLoader tags found: " + jsTag.length);
+        for (var i = 0; i < jsTag.length; i++) {
+            console.log("::: jsLoader #" + i + " :::");
+
+            if (jsTag[i].hasAttribute("data-src")) {
+                console.log("src[" + jsTag[i].dataset.src.split(",").length + "]: " + jsTag[i].dataset.src);
+
+                if (jsTag[i].hasAttribute("data-options")) {
+                    console.log("Options set: ");
+                    var options = new jsLoader;
+                    var optionsArray = jsTag[i].dataset.options.split(",");
+                    for (var o = 0; o < optionsArray.length; o++) {
+                        console.log(optionsArray[o].trim());
+
+                        var option = optionsArray[o].split(":")[0].trim();
+                        var value = optionsArray[o].split(":")[1].trim();
+
+                        switch (option) {
+                            case "directory":
+                                options.directory = value;
+                                break;
+                            case "param":
+                                options.param = value;
+                                break;
+                            case "position":
+                                options.position = value;
+                                break;
+                            case "allowNoExt":
+                                options.allowNoExt = !!((value === "true"));
+                                break;
+                            case "allowURL":
+                                options.allowURL = !!((value === "true"));
+                                break;
+                            default:
+                        }
+                    }
+                    console.log(options);
+                }
+
+            } else {
+                console.log("No source attribute (data-src) set");
+                console.log("jsTag attributes set: ");
+                console.log(jsTag[i].dataset);
+            }
+        }
+    },
+
+    tag: function () {
+        "use strict";
+        var jsTag = document.querySelectorAll('[data-type="jsLoader"]');
+
+        for (var i = 0; i < jsTag.length; i++) {
+
+            if (jsTag[i].hasAttribute("data-src")) {
+
+                if (jsTag[i].hasAttribute("data-options")) {
+                    var options = jsLoader.options;
+                    var optionsArray = jsTag[i].dataset.options.split(",");
+                    for (var o = 0; o < optionsArray.length; o++) {
+
+                        var option = optionsArray[o].split(":")[0].trim();
+                        var value = optionsArray[o].split(":")[1].trim();
+
+                        switch (option) {
+                            case "directory":
+                                options.directory = value;
+                                break;
+                            case "param":
+                                options.param = value;
+                                break;
+                            case "position":
+                                options.position = value;
+                                break;
+                            case "allowNoExt":
+                                options.allowNoExt = !!((value === "true"));
+                                break;
+                            case "allowURL":
+                                options.allowURL = !!((value === "true"));
+                                break;
+                            default:
+                        }
+                    }
+                }
+
+                var jsDir = "";
+                if (options.directory !== "") {
+                    jsDir = (options.directory.endsWith("/")) ? options.directory + "" : options.directory + "/";
+                }
+
+                var jsList = jsTag[i].dataset.src.split(",");
+
+                for (var k = 0; k < jsList.length; k++) {
+                    if (jsList[k] !== undefined) {
+                        if (jsList[k] && options.allowURL) {
+
+                        } else {
+                            if (jsList[k].endsWith(".js") && options.allowNoExt) {
+                                jsList[k] = jsList[k].substring(0, jsList[k].length - 3);
+                            }
+                        }
+
+                        var jsResult = (options.allowNoExt) ? jsDir + jsList[k].trim() + ".js" : jsDir + jsList[k].trim() + "";
+                        var s = document.createElement("script");
+                        s.src = jsResult;
+                        head.appendChild(s);
+                    }
+                }
+            }
+            head.removeChild(jsTag[i]);
+        }
+    },
+
+    load: function () {
+
+    }
+};
+console.log(jsLoader.options);
